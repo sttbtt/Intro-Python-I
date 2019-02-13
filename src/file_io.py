f7421bfdf49297495a6ae9b8ad66217fe3291502
Scott Bennett <stt.btt@gmail.com>
2019-02-12 19:45:22 -0500

    file_io done

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('foo.txt')
+with open('foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +21,5 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.text', 'w')
+f.write('I once caught a bolt of lighting,\nCursed the day I let it go!\nFrom a Pearl Jam song, but a good tagline!')
