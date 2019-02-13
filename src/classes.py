c7ce0d79abad13d8dae367db9badd1ab5c031763
Scott Bennett <stt.btt@gmail.com>
2019-02-13 09:27:10 -0500

    classes done

--- src/classes.py
+++ src/classes.py
@@ -17,7 +17,7 @@ class Waypoint(LatLon):
         self.name = name
 
     def __str__(self):
-        return '\"%s\", %f, %f' % (self.name, self.lat, self.lon)
+        return f'\"{self.name}\", {self.lat}, {self.lon}'
 
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
@@ -31,8 +31,7 @@ class Geocache(Waypoint):
         self.size = size
 
     def __str__(self):
-        return '\"%s\", diff %.1f, size %d, %f, %f' % (self.name, self.difficulty, self.size, self.lat, self.lon)
-    
+        return f'\"{self.name}", diff {self.difficulty}, size {self.size}, {self.lat}, {self.lon}'
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
